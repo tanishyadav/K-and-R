a0691d11147d8e06187368b8e30d993dcc47f320
Tanish Yadav <tanishyadav.jpr@gmail.com>
2022-05-28 13:35:20 +0530

    Add: 1: 5.c

---
+++ 1/5.c
@@ -0,0 +1,15 @@
+/* Exercise 1-5 */
+
+#include <stdio.h>
+
+/* print raverse Fahrenehit-Celsius table */
+
+int main(void)
+{
+	int fahr;
+
+	for (fahr = 300; fahr >= 0; fahr = fahr - 20)
+		printf("%3d %6.1f\n", fahr, (5.0/9.0)*(fahr-32));
+
+	return 0;
+}
