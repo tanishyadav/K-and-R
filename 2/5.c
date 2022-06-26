bd61611669aa4adc3393bfa5fcc4a7dfeec0eeb3
Tanish Yadav <tanishyadav.jpr@gmail.com>
2022-06-26 20:28:33 +0530

    Update 2: 5.c

--- 2/5.c
+++ 2/5.c
@@ -22,18 +22,10 @@ int any(char s1[], char s2[])
 {
 	int i;
 	int j;
-	int r;
-	int brk;
 
-	r = -1;
-	brk = 0;
-	for (i = 0; s1[i] != '\0' && brk == 0; i++) {
-		for (j = 0; s2[j] != '\0' && brk == 0; j++) {
-			if (s2[j] == s1[i]) {
-				brk = 1;
-				r = i;
-			}
-		}
-	}
-	return r;
+	for (i = 0; s1[i] != '\0'; i++)
+		for (j = 0; s2[j] != '\0'; j++)
+			if (s2[j] == s1[i])
+				return i;
+	return -1;
 }
